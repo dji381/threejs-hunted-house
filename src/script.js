3d27c3b49bae81c698d1fa4127e1764c02306725
djihad <djihad.khelifa@jeresiliemoncontrat.com>
2024-03-12 11:43:54 +0100

    three js starter

---
+++ src/script.js
@@ -0,0 +1,66 @@
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+const handleResize = ()=>{
+      // Update sizes
+      sizes.width = window.innerWidth
+      sizes.height = window.innerHeight
+  
+      // Update camera
+      camera.aspect = sizes.width / sizes.height
+      camera.updateProjectionMatrix()
+  
+      // Update renderer (canva inner size or resolution)
+      renderer.setSize(sizes.width, sizes.height)
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+      
+}
+
+window.addEventListener("resize",handleResize)
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+function render(time) {
+    time *= 0.001;  // convert time to seconds
+    
+    mesh.rotation.x = time;
+    mesh.rotation.y = time;
+   
+    renderer.render(scene, camera);
+   
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
